e7342ce082599dee728e090aa28974b9cc269e4e
miguelfiais <miguel.fiais10@gmail.com>
2023-08-23 18:13:21 -0300

    feat: add responsive design on cat page

--- src/app/cat/[catId]/components/SkillCat.tsx
+++ src/app/cat/[catId]/components/SkillCat.tsx
@@ -5,30 +5,32 @@ interface ISkillCatProps {
 const SkillCat = ({ nameSkill, quantity }: ISkillCatProps) => {
   return (
     <div className="flex items-center ">
-      <p className="text-xs font-bold w-32">{nameSkill}: </p>
+      <p className="text-xs font-bold w-32 md:text-sm md:w-36 lg:text-base lg:w-48">
+        {nameSkill}:
+      </p>
       <div className="flex gap-2">
         <div
-          className={`w-8 h-3  rounded-full ${
+          className={`w-8 h-3  rounded-full md:w-12 lg:w-16 ${
             quantity >= 1 ? 'bg-amber-950' : 'bg-gray-200'
           }`}
         ></div>
         <div
-          className={`w-8 h-3  rounded-full ${
+          className={`w-8 h-3  rounded-full md:w-12 lg:w-16 ${
             quantity >= 2 ? 'bg-amber-950' : 'bg-gray-200'
           }`}
         ></div>
         <div
-          className={`w-8 h-3  rounded-full ${
+          className={`w-8 h-3  rounded-full md:w-12 lg:w-16 ${
             quantity >= 3 ? 'bg-amber-950' : 'bg-gray-200'
           }`}
         ></div>
         <div
-          className={`w-8 h-3  rounded-full ${
+          className={`w-8 h-3  rounded-full md:w-12 lg:w-16 ${
             quantity >= 4 ? 'bg-amber-950' : 'bg-gray-200'
           }`}
         ></div>
         <div
-          className={`w-8 h-3  rounded-full ${
+          className={`w-8 h-3  rounded-full md:w-12 lg:w-16 ${
             quantity >= 5 ? 'bg-amber-950' : 'bg-gray-200'
           }`}
         ></div>
--- src/app/cat/[catId]/page.tsx
+++ src/app/cat/[catId]/page.tsx
@@ -36,78 +36,82 @@ const Cat = async ({ params }: { params: { catId: string } }) => {
 
   return (
     <div className="container p-5 min-h-[calc(100vh-224.48px)] mx-auto">
-      <div>
+      <div className="md:flex items-start gap-10 lg:gap-14">
         <Image
           src={aboutBreed.url}
           alt={aboutBreed.breeds[0].name}
           width={250}
           height={250}
-          className="rounded-lg mb-5"
+          className="rounded-lg mb-5 lg:w-80"
         />
-
-        <h2 className="text-3xl font-bold text-amber-950">
-          {aboutBreed.breeds[0].name}
-        </h2>
-        <p className="mt-5 text-xs font-medium">
-          {aboutBreed.breeds[0].description}
-        </p>
-        <div className="flex flex-col gap-4 mt-5">
-          <p className="text-xs">
-            <span className="font-bold">Temperament: </span>
-            {aboutBreed.breeds[0].temperament}
+        <div>
+          <h2 className="text-3xl font-bold text-amber-950 md:text-4xl">
+            {aboutBreed.breeds[0].name}
+          </h2>
+          <p className="mt-5 text-xs font-medium md:text-base lg:text-lg">
+            {aboutBreed.breeds[0].description}
           </p>
-          <p className="text-xs">
-            <span className="font-bold">Origin: </span>
-            {aboutBreed.breeds[0].origin}
-          </p>
-          <p className="text-xs">
-            <span className="font-bold">Life Span: </span>
-            {aboutBreed.breeds[0].life_span} years
-          </p>
-          <SkillCat
-            nameSkill="Adaptability"
-            quantity={aboutBreed.breeds[0].adaptability}
-          />
-          <SkillCat
-            nameSkill="Affection level"
-            quantity={aboutBreed.breeds[0].affection_level}
-          />
-          <SkillCat
-            nameSkill="Child Friendly"
-            quantity={aboutBreed.breeds[0].child_friendly}
-          />
-          <SkillCat
-            nameSkill="Grooming"
-            quantity={aboutBreed.breeds[0].grooming}
-          />
-          <SkillCat
-            nameSkill="Intelligence"
-            quantity={aboutBreed.breeds[0].intelligence}
-          />
-          <SkillCat
-            nameSkill="Health issues"
-            quantity={aboutBreed.breeds[0].health_issues}
-          />
-          <SkillCat
-            nameSkill="Social needs"
-            quantity={aboutBreed.breeds[0].social_needs}
-          />
-          <SkillCat
-            nameSkill="Stranger friendly"
-            quantity={aboutBreed.breeds[0].stranger_friendly}
-          />
+          <div className="flex flex-col gap-4 mt-5 text-xs md:text-base">
+            <p>
+              <span className="font-bold">Temperament: </span>
+              {aboutBreed.breeds[0].temperament}
+            </p>
+            <p>
+              <span className="font-bold">Origin: </span>
+              {aboutBreed.breeds[0].origin}
+            </p>
+            <p>
+              <span className="font-bold">Life Span: </span>
+              {aboutBreed.breeds[0].life_span} years
+            </p>
+            <SkillCat
+              nameSkill="Adaptability"
+              quantity={aboutBreed.breeds[0].adaptability}
+            />
+            <SkillCat
+              nameSkill="Affection level"
+              quantity={aboutBreed.breeds[0].affection_level}
+            />
+            <SkillCat
+              nameSkill="Child Friendly"
+              quantity={aboutBreed.breeds[0].child_friendly}
+            />
+            <SkillCat
+              nameSkill="Grooming"
+              quantity={aboutBreed.breeds[0].grooming}
+            />
+            <SkillCat
+              nameSkill="Intelligence"
+              quantity={aboutBreed.breeds[0].intelligence}
+            />
+            <SkillCat
+              nameSkill="Health issues"
+              quantity={aboutBreed.breeds[0].health_issues}
+            />
+            <SkillCat
+              nameSkill="Social needs"
+              quantity={aboutBreed.breeds[0].social_needs}
+            />
+            <SkillCat
+              nameSkill="Stranger friendly"
+              quantity={aboutBreed.breeds[0].stranger_friendly}
+            />
+          </div>
         </div>
       </div>
       <h3 className="text-2xl  font-semibold text-amber-950 mt-8">
         Other photos
       </h3>
-      <div className="flex flex-wrap gap-3 mt-5">
+      <div className="grid grid-cols-2 gap-3 mt-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {imagesBreed &&
           imagesBreed.map((breed: any) => (
-            <div key={breed.id} className="relative h-[150px] w-[150px]">
+            <div
+              key={breed.id}
+              className="relative h-[150px] w-full sm:h-[180px] md:h-[220px] lg:h-[260px]"
+            >
               <Image
                 src={breed.url}
-                alt="Abys"
+                alt={breed.breeds[0].name}
                 fill
                 objectFit="cover"
                 className="rounded-lg"
