3d1fc8b0a16cd04cb25f14b11dc3654b1fc5636c
miguelfiais <miguel.fiais10@gmail.com>
2023-08-23 10:53:25 -0300

    feat: add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <footer className="container px-5 mt-5">
+      <div className="bg-black p-5 rounded-3xl rounded-b-none">
+        <Image
+          src={'/CatwikiLogoFooter.svg'}
+          alt="logo"
+          width={100}
+          height={33.59}
+        />
+        <div className="text-white flex items-center mt-5 gap-1">
+          <span className="text-lg">© </span>
+          <p className="text-xs ">
+            created by{' '}
+            <a
+              href="https://github.com/miguelfiais"
+              target="_blank"
+              className="font-bold"
+            >
+              Miguel Fiais{' '}
+            </a>
+            - devChallenge.io 2021
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import './globals.css'
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
